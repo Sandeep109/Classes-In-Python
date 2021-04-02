a1bf8a31e9786ff891ab5427cc40a41b8fe77c9a
Sandeep109 <63722540+Sandeep109@users.noreply.github.com>
2021-04-02 17:35:43 +0530

    Add files via upload

---
+++ bankatm.py
@@ -0,0 +1,20 @@
+class atm:
+    def __init__(self, card_number, pin_number):
+        self.card_number = ""
+        self.pin_number = ""
+
+    def withdrawal():
+        print("Withdraw you cash")
+        #other code functionality here
+
+    def balanceEnquiry():
+        print("Enquire your account balance")
+        #other code functionality here
+
+acc_num = input("Enter Your Account Number :- ")
+acc_pin = input("Enter Your Pin :- ")
+
+bankatm = atm(acc_num, acc_pin)
+
+print(bankatm.card_number)
+
